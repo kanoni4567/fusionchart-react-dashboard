171ce5079c17224bff29378cd9395351b6ce30be
Shanyu Cui <kanoni4567@gmail.com>
2019-03-22 04:09:41 -0700

    added cards

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import RealtimeChart from "./components/RealtimeChart";
 import HistoricalChart from "./components/HistoricalChart";
+import Card from "./components/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -13,6 +14,19 @@ class App extends Component {
         <div className='container'>
           <RealtimeChart />
           <HistoricalChart />
+
+          <div className='col-12 mb-3'>
+            <div className='card-deck custom-card-deck'>
+              <Card header='BEST TIME TO PICK UP WASTE' value='3:15pm' />
+              <Card header='LAST TIME EMPTIED' value='9:00am' />
+              <Card header='DIVERSION RATE' value='84%' />
+              <Card
+                header='MOST THROWN OUT ITEM'
+                value='Starbucks Coffe Cup'
+                src='https://cdn1.iconfinder.com/data/icons/food-drink-7/100/FD_align-08-512.png'
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -5,15 +5,13 @@ const Card = props => {
   return (
     <div className='card mr-0 custom-card'>
       <div className='card-body'>
-        <img
-          src={props.src}
-          alt={props.src}
-          className='img-responsive float-right'
-        />
+        {props.src ? (
+          <img src={props.src} className='img-responsive float-right' />
+        ) : null}
         <h6 className='card-title mb-4 '>{props.header} </h6>
-        <h2 className='mb-1 text-primary'>${value}</h2>
+        <h2 className='mb-1 text-primary'>{value}</h2>
         <p className='card-text'>
-          <small className='text-muted'>{props.label}</small>
+          <small className='text-muted'>{props.label || ""}</small>
         </p>
       </div>
     </div>
